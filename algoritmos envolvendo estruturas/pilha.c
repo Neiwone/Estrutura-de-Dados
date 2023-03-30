d8967392db699ecb07896fff3cfacc24dd18127b
vinícius <98852360+Neiwone@users.noreply.github.com>
2023-03-29 21:54:37 -0300

    Add files via upload

---
+++ algoritmos envolvendo estruturas/pilha.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack.h"
+
+
+int main() {
+
+    int opcao, valor;
+    stack * pilha;
+    stack * pilha1;
+
+
+    // Definindo pilha para possivel união.
+    pilha1->topo = criar_pilha(pilha1);
+
+    if(criar_pilha(pilha) != -1) {
+        printf("nao foi possivel criar a pilha. \n");
+        return 0;
+    }
+
+    while(1)
+    {
+    
+        printf("\n1 - Empilhar\n");
+        printf("2 - Desimpilhar\n");
+        printf("3 - Tamanho da pilha\n");
+        printf("4 - Imprimir pilha\n");
+        printf("5 - Valor do topo\n");
+        printf("6 - Inverter pilha\n");
+        printf("7 - Unir duas pilhas\n");
+        printf("8 - Ordenar pilha\n");
+        printf("0 - Sair\n\n");
+        printf("opcao: ");
+
+        scanf("%d", &opcao); 
+        fflush(stdin);
+        printf("\n");
+               
+        switch(opcao)
+        {
+            case 1:
+                printf("Digite o valor que deseja empilhar: ");
+                scanf("%d", &valor);
+                fflush(stdin);
+
+                empilhar(pilha, valor);
+                printar_pilha(pilha, 0);
+                break;
+            case 2:
+                if (pilha_vazia(pilha) == true) {
+                    printf("pilha vazia. :( \n");
+                    break;
+                }
+
+                printf("o elemento %d foi desempilhado. \n", desempilhar(pilha));
+                printar_pilha(pilha, 0);
+                break;      
+
+            case 3:
+                printf("A pilha possui %d elementos.\n", pilha->topo + 1);
+                break; 
+            
+            case 4:
+                printar_pilha(pilha, 0);
+                break; 
+
+            case 5:
+                if(pilha_vazia(pilha) == false) printf("O elemento do topo e: %d.\n", topo_valor(pilha));
+                else printf("pilha vazia.\n");
+                break; 
+            case 6:
+                pilha = inverte_pilha(pilha);
+                printar_pilha(pilha, 0);
+                break;
+            case 7:
+                printf("digite os valores para a segunda pilha(Max: 10): ");
+                while (scanf("%d", &valor) == 1) {
+                    empilhar(pilha1, valor);
+                }
+                fflush(stdin);
+                printar_pilha(unir_pilhas(pilha,pilha1), 0);
+                break; 
+            case 8:
+                printar_pilha(ordenar_pilha(pilha), 0);
+                break; 
+            case 0:
+                exit(0);
+                break; 
+
+            default:
+                printf("insira um valor valido\n");
+                break;    
+        }
+    }    
+}   
+    
---
+++ algoritmos envolvendo estruturas/stack.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+int criar_pilha(stack * pilha) {
+
+    pilha->topo = -1;
+    return pilha->topo;
+}
+
+bool pilha_vazia(stack * pilha) {
+    return (pilha->topo == -1) ? true : false;
+}
+
+bool pilha_cheia(stack * pilha) {
+    return (pilha->topo + 1 < MAXN) ? false : true;
+}
+
+bool empilhar(stack * pilha, int valor) {
+    
+    if (pilha_cheia(pilha) == true) {
+        printf("pilha cheia. :(\n");
+        return false;
+    }
+
+    pilha->topo+=1;
+    pilha->elemento[pilha->topo] = valor;
+    return true;
+}
+
+int desempilhar(stack * pilha) {
+
+    int aux = pilha->elemento[pilha->topo];
+    pilha->topo--;
+    return aux;
+}
+
+int printar_pilha(stack * pilha, int i) {
+    
+    if(pilha_vazia(pilha) == true) return printf("a pilha esta vazia!\n");
+    if(i == 0) printf("elementos da pilha: ");
+    printf("%d", pilha->elemento[i]);
+    if(i == pilha->topo) return printf(".\n");
+    printf(", "); 
+    printar_pilha(pilha, i+1);
+}
+
+int topo_valor(stack * pilha) {
+
+    if (pilha_vazia(pilha) == false) return pilha->elemento[pilha->topo];
+    return printf("pilha vazia.\n");
+}
+
+int tamanho_pilha(stack * pilha) {
+
+    if (pilha_vazia(pilha) == false) return pilha->topo + 1;
+    return printf("pilha vazia.\n");
+}
+
+stack * inverte_pilha(stack * pilha) {
+    stack * pilhainvertida;
+    criar_pilha(pilhainvertida);
+    int tamanho = pilha->topo + 1;
+    for (int i = 0; i < tamanho; i++) empilhar(pilhainvertida,desempilhar(pilha));
+    return pilhainvertida;
+}
+
+stack * unir_pilhas(stack * pilha1, stack * pilha2) {
+    int tamanho = pilha2->topo + 1;
+    for (int i = 0; i < tamanho; i++) empilhar(pilha1,pilha2->elemento[i]);
+    return pilha1;
+}
+
+stack * ordenar_pilha(stack * pilha) {
+    int aux;
+    int tamanho = tamanho_pilha(pilha);
+    for (int contador = 1; contador < tamanho; contador++) {
+        for (int i = 0; i < tamanho - 1; i++) {
+            if (pilha->elemento[i] < pilha->elemento[i + 1]) {
+                aux = pilha->elemento[i];
+                pilha->elemento[i] = pilha->elemento[i + 1];
+                pilha->elemento[i + 1] = aux;
+            }
+        }
+    }
+    return pilha;   
+}
